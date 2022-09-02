89216c406350fb88880bec8c8bedc37e96d52a1f
Zack Walsh <zachary.walsh@opusivs.com>
2022-09-02 09:56:46 -0400

    Restructure repo
    Moved all contents to root directory of the repo.

---
+++ LogArchiving/LogArchiveConfiguration.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel;
+using System.IO.Compression;
+using Newtonsoft.Json;
+
+namespace SharpLogger.LogArchiving
+{
+    /// <summary>
+    /// Class which contains configuration type info for an archive session
+    /// </summary>
+    public class LogArchiveConfiguration
+    {
+        // Configuration values
+        [DefaultValue(false)] 
+        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Populate)]
+        public bool ProgressToConsole;
+        
+        // Archive path value
+        public string LogArchivePath;
+
+        // Archive set info.
+        [DefaultValue(15)] 
+        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Populate)]  
+        public int ArchiveFileSetSize;
+        [DefaultValue(20)] 
+        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Populate)]
+        public int ArchiveOnFileCount;
+
+        // Archive Cleanup File Size Trigger
+        [DefaultValue(50)]
+        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Populate)]
+        public int ArchiveCleanupFileCount;
+
+        // Compression configuration
+        [JsonIgnore] public CompressionLevel CompressionLevel;
+        [JsonIgnore] public CompressionType CompressionStyle;
+        [JsonProperty("CompressionLevel")] private string _compressionLevel => CompressionLevel.ToString();
+        [JsonProperty("CompressionStyle")] private string _compressionStyle => CompressionStyle.ToString();
+
+        // -----------------------------------------------------------------------------------
+
+        /// <summary>
+        /// Builds a new instance of a log archive configuration object.
+        /// </summary>
+        public LogArchiveConfiguration() { }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,25 @@
+﻿// Using calls for Assembly Info updating
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Resources;
+
+// General Information
+[assembly: AssemblyTitle("SharpLogger")]
+[assembly: AssemblyDescription("A C# Logger that really does the most")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("MEAT Inc")]
+[assembly: AssemblyProduct("SharpLogger")]
+[assembly: AssemblyCopyright("Copyright ©MEAT Inc 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Internal Visibility for testing
+[assembly: ComVisible(false)]
+[assembly: Guid("909e0c64-5b20-4267-b57c-87a29383525a")]
+
+// Version information
+[assembly: AssemblyVersion("1.9.0.123")]
+[assembly: AssemblyFileVersion("1.9.0.123")]
+[assembly: NeutralResourcesLanguageAttribute( "en-US" )]
+
